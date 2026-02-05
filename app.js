505a30dd1f5a7cfa096da162bb30bdd190c1cb1d
Francis-annor <annorfrancis55@gmail.com>
2026-02-05 15:18:34 +0000

    Using statusCode and setHeader instead of writeHead

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const hostname = "127.0.0.1";
 const PORT = 3000;
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    // res.writeHead(200, {'Content-Type':'text/plain'});
     res.end("Hello From Server NodeJS");
 });
 
-server.listen(3000, '127.0.0.1', () => {
+server.listen(PORT, hostname, () => {
     console.log("Server Successfully Started");
     console.log(`Server running on: http://'${hostname}:${PORT}`);
 });
